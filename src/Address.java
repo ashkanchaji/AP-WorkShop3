e0a491a3c74076d35a37ed0d32aac4184a9fda80
Ashkan Chaji <achaji2563@gmail.com>
2024-03-11 14:28:45 +0330

    added Address

---
+++ src/Address.java
@@ -0,0 +1,9 @@
+public class Address {
+    private String zipCode;
+    private String country;
+    private  String city;
+
+    public String toString (){
+        return "ZipCode: " + zipCode + "\nCountry: " + country + "\nCity: " + city;
+    }
+}
