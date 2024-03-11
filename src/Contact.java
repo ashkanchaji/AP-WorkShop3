a4d3450d1d0a91c4bb42add3280b7075dd874195
Ashkan Chaji <achaji2563@gmail.com>
2024-03-11 14:29:58 +0330

    added Contact

---
+++ src/Contact.java
@@ -0,0 +1,32 @@
+public class Contact {
+    private String group;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private PhoneNumber phoneNumber;
+    private Address address;
+
+    public String getGroup() {
+        return group;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public PhoneNumber getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+}
